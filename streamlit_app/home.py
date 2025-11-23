54bf2c79512cfe21d4011edb361d4f04ec7fd80a
AuraFrizzati <frizzatia@gmail.com>
2025-11-23 19:35:15 +0000

    main app setup

---
+++ streamlit_app/home.py
@@ -0,0 +1,23 @@
+## streamlit run viz_dashboard.py
+import streamlit as st
+
+# streamlit page config
+st.set_page_config(
+    page_title="Net Zero Co-Benefits in Wales",  # the page title shown in the browser tab
+    page_icon=":bar_chart:",  # the page favicon shown in the browser tab
+    layout="wide",  # page layout : use the entire screen
+)
+# page title
+st.title("Net Zero Co-Benefits in Wales :deciduous_tree:")
+
+# 'About dashboard' section
+with st.expander('About this Data Viz'):
+    st.header("About this Data Viz")
+    st.markdown("""
+                **SOME TEXT** [emphasise the importance of explaining net zero 
+             cobenefits to population]
+                """)
+    # iris_image = Image.open("imgs/iris_species.png")
+    # st.image(iris_image)
+
+# load Welsh data
