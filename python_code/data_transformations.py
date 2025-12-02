e56df4d5e53a50c3764f98708679c3b410f71c51
AuraFrizzati <frizzatia@gmail.com>
2025-12-02 21:27:38 +0000

    various

--- python_code/data_transformations.py
+++ python_code/data_transformations.py
@@ -1,34 +1,59 @@
 import pandas as pd
 import os
-
-def filter_cardiff(df, lookup, csv_name = "Cardiff_lookup"):
+# 
+def filter_cardiff(csv_name = "Cardiff_lookup_wimd"):
     try:
         print("function filter_cardiff started ...")
-        # local_authority == "Cardiff"
+        print("reading level 3 data...")
+        level_3 =  pd.read_excel("data/Level_3.xlsx")
+        print("reading lookup data...")
+        lookup = pd.read_excel("data/lookups.xlsx")
+        wimd_url = "https://www.gov.wales/sites/default/files/statistics-and-research/2025-11/wimd-2025-index-and-domain-ranks-by-small-area.ods"
+        wimd_df = pd.read_excel(wimd_url, engine='odf', 
+                                sheet_name='Deciles_quintiles_quartiles'
+                                ,skiprows=3)
+
         cardiff_codes = lookup.loc[lookup['local_authority'] == "Cardiff", 'small_area'].tolist()
         df = df[df['small_area'].isin(cardiff_codes)]
+        
+        print("merge with lookup ...")
         df_lookup = pd.merge(df, lookup, how='inner', on='small_area')
         # Remove 'Unnamed' and 'nation' columns, move 'local_authority' as first
         cols = [col for col in df_lookup.columns if col not in ['Unnamed: 4', 'nation']]
         cols = ['local_authority'] + [col for col in cols if col != 'local_authority']
         df_lookup = df_lookup[cols]
-        df_lookup.to_csv(f"data/{csv_name}.csv", index=False)
+
+        # merge with wimd data
+        print("merge with WIMD data ...")
+        df_lookup_wimd = pd.merge(df_lookup, wimd_df, how='inner', left_on='small_area', right_on = 'LSOA code')
+        
+        df_lookup_wimd = df_lookup_wimd.drop(columns=[
+            'local_authority',
+            'small_area',
+            'Local Authority name (Eng)',
+            'WIMD 2025 overall quartile',
+            'WIMD 2025 overall deprivation group'
+            ])
+
+
+        df_lookup_wimd.to_csv(f"data/{csv_name}.csv", index=False)
     except Exception as e:
         print(f"An error occurred in filter_cardiff: {e}")
 
     print("function filter_cardiff completed")
 
-    return df_lookup 
+    # return df_lookup 
 
 def main():
     print(os.getcwd())
-    # import level 3 raw data
-    level_3 =  pd.read_excel("data/Level_3.xlsx")
-    lookup = pd.read_excel("data/lookups.xlsx")
-    filter_cardiff(level_3, lookup)
+    # filter_cardiff()
+    cardiff_lookup_wimd = pd.read_csv("data/Cardiff_lookup_wimd.csv")
+    print(cardiff_lookup_wimd.head())
+    print(cardiff_lookup_wimd.dtypes)
 
-    return
 
+
+    
 if __name__ == "__main__":
     main()
 
